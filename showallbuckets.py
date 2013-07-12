4e8bbc6cbc6959065267b31160afd2a2a2fcb33e
Nick Bond <nickabond@gmail.com>
2013-07-12 06:50:35 -0500

    Update showallbuckets.py

--- showallbuckets.py
+++ showallbuckets.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# Author: Nick Bond
+# Purpose: This script shows all keys within a particular S3 bucket. 
 import MySQLdb as mdb
 import boto 
 import sys
@@ -12,28 +14,17 @@ def object_creator():
 	return generator	
 
 
-#objname = "cumulus://svc.uc.futuregrid.org:8888/ANLTowerData/%s" % object_creator()
-
 filename = 'mysqlbackup.sql'
-
-access_key_id = 'Ua1QUBp9XFeApwMv0sSXk'
-secret_access_key = 'lB8idOCDF47Isxo3oF2guUprg6BXIveKugpiLQr6Kw'
+access_key_id = 'access_key_id'
+secret_access_key = 'secret_access_key'
 
 conn=boto.s3.connection.S3Connection(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, #Connecting to Cumulus
-is_secure=False, port=8888, host='svc.uc.futuregrid.org',
+is_secure=False, port=8888, host='hostname',
 debug=0, https_connection_factory=None, calling_format = boto.s3.connection.OrdinaryCallingFormat())
 
 bucket = conn.get_bucket('anltowerdata') 
-
-
-###key = bucket.new_key('mysqlbackup.sql')
-#key.set_contents_from_string('yes,indeed')
-###key.set_contents_from_filename(filename)
-###key = bucket.get_key('mysqlbackup.sql')
-###key.set_canned_acl('public-read')
 key = bucket.get_key('mysqlbackup.sql')
-#key_url = key.generate_url(0, query_auth=False, force_http=True)
-#print key_url
+
 for key in bucket.list():
         print "{name}\t{size}\t{modified}".format(
                 name = key.name,
