638b5cd848cefe446d124cd769b937bcc7e22ebe
Nick Bond <nickabond@gmail.com>
2013-07-12 06:35:31 -0500

    Update kill.py

--- kill.py
+++ kill.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-
+# Author: Nick Bond
+# Purpose: This is a simple cleanup script that makes sure that 
+#          no lingering python processes are bogging down the virtual
+#          machine. 
 from subprocess import call
 
 call(['pkill','-9','python'])
