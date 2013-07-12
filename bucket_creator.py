890a7d9de1e7298836cbb9a1db4ad00fa745872c
Nick Bond <nickabond@gmail.com>
2013-07-12 06:20:47 -0500

    Update bucket_creator.py

--- bucket_creator.py
+++ bucket_creator.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+#Author: Nick Bond
+#Purpose: This script allows the user to connect to an S3 cloud storage source
+#	  and then create a new bucket and key. After that the user is able to
+#	  upload a file and append the bucket with the new key that houses their
+#	  file. A MySQL dump is used for this example
+
 import MySQLdb as mdb
 import boto 
 import sys
@@ -12,22 +18,20 @@ def object_creator():
 	return generator	
 
 
-#objname = "cumulus://svc.uc.futuregrid.org:8888/ANLTowerData/%s" % object_creator()
-
 filename = 'mysqlbackup.sql'
 
-access_key_id = 'Ua1QUBp9XFeApwMv0sSXk'
-secret_access_key = 'lB8idOCDF47Isxo3oF2guUprg6BXIveKugpiLQr6Kw'
+access_key_id = 'access_key_id'
+secret_access_key = 'secret_key'
+
+##Connecting to cloud storage##
 
 conn=boto.s3.connection.S3Connection(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, #Connecting to Cumulus
-is_secure=False, port=8888, host='svc.uc.futuregrid.org',
+is_secure=False, port=8888, host='hostname',
 debug=0, https_connection_factory=None, calling_format = boto.s3.connection.OrdinaryCallingFormat())
 
+##Connecting to an existing bucket and creating a new key##
 bucket = conn.get_bucket('anltowerdata') 
-
-
 key = bucket.new_key('mysqlbackup.sql')
-#key.set_contents_from_string('yes,indeed')
 key.set_contents_from_filename(filename)
 key = bucket.get_key('mysqlbackup.sql')
 key.set_canned_acl('public-read')
